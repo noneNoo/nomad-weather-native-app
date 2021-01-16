db31f22b8ac9d4787bdca3ab7ccb33385efe8a2d
noneno <noneno@kakao.com>
2021-01-16 21:54:48 +0900

    Changed Component Type and Add getLocation method

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 // import { StyleSheet, Text, View } from 'react-native';
 import Loading from './Loading';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  getLocation = async () => {
+    // 사용자의 현재 좌표를 가져온다
+    const location = Location.getCurrentPositionAsync();
+    console.log(location);
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    return <Loading />;
+  }
 }

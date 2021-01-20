4304a5096486550d189c154f5fab23143363549e
noneno <noneno@kakao.com>
2021-01-20 19:53:59 +0900

    Asking for Permissions

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import { Alert } from 'react-native';
 // import { StyleSheet, Text, View } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
   getLocation = async () => {
-    // 사용자의 현재 좌표를 가져온다
-    const location = Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      // 사용자 permissions 요청
+      await Location.requestPermissionsAsync();
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+
+      this.setState({ isLoading: false });
+
+      //받아오기 실패했을 경우
+    } catch (error) {
+      Alert.alert("Can't find you.", 'So sad');
+    }
   };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
